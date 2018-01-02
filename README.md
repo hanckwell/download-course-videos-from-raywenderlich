8c2563f6440816cd96962072d95033f43926862c
Siyao Meng <siyaom@andrew.cmu.edu>
2018-01-02 14:10:47 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # RayWenderlich-Crawler
+
+
+## Usage
+
+Requires subscription to download course videos from RayWenderlich.
+
+1. Open Chrome Developer Tools, switch to 'Network' tab.
+
+2. Access a video course page on raywenderlich.com (e.g. `https://videos.raywenderlich.com/courses/91-your-first-swift-4-ios-11-app/lessons/1`).
+
+3. Right click on the first GET request, select Copy -> Copy as cURL. The copied value should look like:
+
+```
+curl 'https://videos.raywenderlich.com/courses/91-your-first-swift-4-ios-11-app/lessons/1' -H 'Pragma: no-cache' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja-JP;q=0.6,ja;q=0.5' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Cache-Control: no-cache' -H 'Cookie: ...' -H 'Connection: keep-alive' --compressed
+```
+
+4. Replace the "..." in rwcrawl.py (variable str_curl) with the copied value.
+
+5. Replace the video course URL (`https://videos.raywenderlich.com/courses/91-your-first-swift-4-ios-11-app/lessons/1` in our example) with `%s`. The result in the Python source code should look like:
+
+```
+str_curl = "curl '%s' -H 'Pragma: no-cache' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja-JP;q=0.6,ja;q=0.5' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Cache-Control: no-cache' -H 'Cookie: ...' -H 'Connection: keep-alive' --compressed"
+```
+
+6. Download the entire course using command:
+
+```
+python3 rwcrawl.py https://videos.raywenderlich.com/courses/91-your-first-swift-4-ios-11-app/lessons/1
+```
+
+You can download other video courses with this command as well, just replace the URL with other courses'.
+
+Enjoy.
+
---
+++ rwcrawl.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import re
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+import requests
+import uncurl
+from urllib.parse import urlparse
+import youtube_dl
+import os
+
+# Tried to add referer in the youtube_dl header to download vimeo videos directly but failed
+#import youtube_dl.utils
+#youtube_dl.utils.std_headers['referer'] = 'https://videos.raywenderlich.com/'
+#print(youtube_dl.utils.std_headers)
+
+# Note: Replace str_curl with your cURL
+str_curl = "..."
+
+vimeo_pfx = "https://player.vimeo.com/video/"
+ydl_opts = {'writesubtitles': True}
+
+
+def get_host(url):
+    data = urlparse(url)
+#    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    return data.netloc
+            
+
+def download_vimeo(vimeo_id):
+    vimeo_url = vimeo_pfx + vimeo_id
+    print(vimeo_url)
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([vimeo_url])
+
+
+def main():
+
+#    response = HtmlResponse(url='https://videos.raywenderlich.com/courses/90-programming-in-swift/lessons/1', **kwargs)
+    cmd_curl = uncurl.parse(str_curl)
+    response = eval(cmd_curl)
+    fmt_curl = cmd_curl.replace(response.url, "%s")
+    body = response.text
+    tables = Selector(text=body).xpath('//ul[@class="lesson-table"]')#.extract()
+    host = "https://" + get_host(response.url)
+    
+    title = Selector(text=body).xpath('//h2[@class="course-title"]/text()').extract_first()
+    
+    try:
+        if not os.path.exists(title):
+            os.makedirs(title)
+        else:
+            print('Already downloaded! Quitting.')
+            exit(0)
+    except OSError:
+        print('FATAL: Error creating directory. ' + directory)
+        exit(-1)
+    
+    fi = open('%s/info.txt' % title, "w")
+    f = open('%s/videos.txt' % title, "w")
+    
+    # http://masnun.com/2016/09/18/python-using-the-requests-module-to-download-large-files-efficiently.html
+    def download_file(url, filename):
+        response = requests.get(url, stream=True)
+        handle = open(title + '/' + filename, "wb")
+        for chunk in response.iter_content(chunk_size=512):
+            if chunk:  # filter out keep-alive new chunks
+                handle.write(chunk)
+    
+    def grab(part, number, curl, time, name):
+        r = eval(curl)
+        body = r.text
+        with open('%s/%s-%s %s.htm' % (title, part, number, name), 'w') as src:
+            src.write(body)
+        fi.write(r.url + '\n')
+        materials = Selector(text=body).xpath('//a[@class="download-materials"]')
+        if materials:
+            materials_filename = materials.xpath('@download').extract_first()
+            materials_url = host + materials.xpath('@href').extract_first()
+            fi.write(materials_url + ' ' + materials_filename + '\n')
+    #        materials = li.xpath('//section[@id="video-info"]')
+    #        print(materials.extract(), materials_filename, materials_url)
+            download_file(materials_url, "%s-%s %s" % (part, number, materials_filename))
+        vimeo_id = Selector(text=body).xpath('//div[@id="vimeo-player"]/@data-vimeo-id').extract_first()
+#        download_vimeo(vimeo_id)
+        vimeo_url = vimeo_pfx + vimeo_id
+        fi.write(vimeo_url + '\n\n')
+        f.write(vimeo_url + '\n')
+    
+    for part, table in enumerate(tables, 1):
+        print("Part %d" % part)
+        for li in table.xpath('li'):
+            number = li.xpath('span[@class="lesson-number"]/text()').extract_first()
+            url = response.url
+            name = li.xpath('span[@class="lesson-name"]/a/text()').extract_first()
+            if not name:  # Trick. Contains class 'active'
+                name = li.xpath('span[@class="lesson-name"]/text()').extract_first()
+            else:
+                url = host + li.xpath('span[@class="lesson-name"]/a/@href').extract_first()
+            time = li.xpath('span[@class="lesson-time"]/text()').extract_first()
+            print(number, url, name, time)
+            fi.write(str(part) + '-' + number + ' ' + name + '\n')
+            grab(part, number, fmt_curl % url, time, name)
+    
+    f.close()
+    fi.close()
+    
+    # Launch youtube-dl
+    print('\nRun the following command to download videos:')
+    print('cd "%s" && youtube-dl -a videos.txt --referer https://videos.raywenderlich.com/ --all-subs -o "%(autonumber)s %(id)s %(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args "-c -j 3 -x 3 -s 3 -k 1M" && for f in *.vtt; do ffmpeg -i $f $f.srt; done"' % title)
+#    os.chdir(title)
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print('Usage: python3 rwcrawl.py [URL]')
+        exit(0)
+        
+    str_curl = str_curl % sys.argv[1]
+    main()
+    
